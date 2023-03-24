8c55264dee21effe9bc445eb63001dd80cf53f77
Hyeon Su Kim <lemonjames96@gmail.com>
2023-03-24 11:34:44 +0900

    Create testcode.py

---
+++ testcode.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import os
+import json
+from typing import List, Dict
+
+
+# Q1. Load a Pandas dataframe using the given Excel file 
+def load_dataframe(path: str) -> pd.DataFrame:
+    # your code here
+    df = pd.DataFrame()
+    return df
+
+# Q2. Convert a specific value 
+def convert_value(df: pd.DataFrame) -> np.array:
+    pass
+
+if __name__ == '__main__':
+    path = ''
+    df = load_dataframe(path)
+
+    
